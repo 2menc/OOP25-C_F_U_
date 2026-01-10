3604d0ca5c7f66900b5cfd3ff48c88bdd293d5b5
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-01-10 16:41:56 +0100

    implemented Enigma

---
+++ src/main/java/it/unibo/impl/templates/EnigmaTemplate.java
@@ -0,0 +1,67 @@
+package it.unibo.impl.templates;
+
+import java.util.Collections;
+import java.util.List;
+
+import it.unibo.api.enigmas.Enigma;
+
+public class EnigmaTemplate implements Enigma{
+
+    private final String id;
+    private boolean completed;
+    private boolean keyInside;
+
+    private final String question;
+    private final String correctOption;
+    private final List<String> options;
+
+    public EnigmaTemplate(final String id, final boolean keyInside, 
+        final String question, final List<String> options, final String correctOption
+    ) {
+        this.id = id;
+        this.completed = false;
+        this.keyInside = keyInside;
+
+        if(! options.contains(correctOption)) {
+            throw new IllegalArgumentException("no such option in the list");
+        }
+        this.question = question;
+        this.options = options;
+        this.correctOption = correctOption;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return this.completed;
+    }
+
+    @Override
+    public boolean hasTheKey() {
+        return this.keyInside;
+    }
+
+    @Override
+    public String getQuestion() {
+        return this.question;
+    }
+
+    @Override
+    public List<String> getOptions() {
+        Collections.shuffle(this.options);
+        return options;
+    }
+
+    @Override
+    public boolean solve(String answer) {
+        if(answer.equals(this.correctOption)) {
+            return true;
+        }
+        return false;
+    }
+
+}
