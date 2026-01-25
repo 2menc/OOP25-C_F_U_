491826ff227b5d6fd657e11a55edd4913252d997
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-01-25 23:27:46 +0100

    implemented enigmas loading and saving system

---
+++ src/main/java/it/unibo/storage/enigma/EnigmaSaveLoad.java
@@ -0,0 +1,52 @@
+package it.unibo.storage.enigma;
+
+import org.yaml.snakeyaml.LoaderOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.inspector.TagInspector;
+
+import it.unibo.api.enigmas.Enigma;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+* enigmas loading system
+ */
+public class EnigmaSaveLoad {
+
+    private final static String filePath = "./src/main/java/it/unibo/storage/enigma/enigmas.yml";
+
+    List<Enigma> enigmas = new ArrayList<>();
+    
+    /**
+     * 0 args constructor
+     */
+    public EnigmaSaveLoad() { }
+
+    /**
+     * loads the enigmas list from {@code enigmas.yml}
+     */
+    public void loadEnigmas() {
+        final LoaderOptions loadOpt = new LoaderOptions();
+        final TagInspector tagInsp = t -> t.getClassName().startsWith("it.unibo");
+        loadOpt.setTagInspector(tagInsp);
+
+        try(final InputStream fis = new FileInputStream(filePath)) {
+        final Yaml yamlFile = new Yaml(new Constructor(List.class, loadOpt));
+            enigmas = yamlFile.load(fis);
+        } catch (final Exception excep) {
+            excep.printStackTrace();
+        }
+    }
+
+    /**
+     * gets the list of the enigmas in the file
+     * @return the list of enigmas
+     */
+    public List<Enigma> getEnigmas() {
+        return this.enigmas;
+    }
+}
---
+++ src/main/java/it/unibo/storage/enigma/EnigmaSaveWrite.java
@@ -0,0 +1,57 @@
+package it.unibo.storage.enigma;
+
+import org.yaml.snakeyaml.Yaml;
+
+import it.unibo.api.enigmas.Enigma;
+import it.unibo.impl.templates.EnigmaTemplate;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * enigmas saving system
+ */
+public class EnigmaSaveWrite {
+
+    private final static String filePath = "./src/main/java/it/unibo/storage/enigma/enigmas.yml";
+
+    private List<Enigma> enigmas = new ArrayList<>();
+
+    /**
+     * 0 arguments constructor
+     */
+    public EnigmaSaveWrite() { }
+
+    /**
+     * adds a new enigma in the list
+     * @param id the id
+     * @param question the question
+     * @param correctOption the correct answer
+     * @param options all options
+     * @param hasTheKey if this enigma has to drop a key when completed
+     */
+    public void addEnigma(final String id, final String question,
+            final String correctOption, final List<String> options, final boolean hasTheKey) {
+
+        if(options.contains(correctOption)) {
+            this.enigmas.add(new EnigmaTemplate(id, hasTheKey, question, options, correctOption));
+        } else {
+            throw new IllegalArgumentException("wrong answers");
+        }
+    }
+
+    /**
+     * writes on the .yaml file the new enigma data
+     */
+    public void writeOnFile() {        
+        try (final FileWriter pw = new FileWriter(filePath)) {            
+            Yaml yamlFile = new Yaml();
+            yamlFile.dump(this.enigmas, pw);
+        } catch (final IOException excep) {
+            excep.printStackTrace();
+        }
+    }
+
+}
