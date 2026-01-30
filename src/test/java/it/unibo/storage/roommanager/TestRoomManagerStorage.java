00c195ed04457f134e8330620331b64691bb56df
Luca Pezzolesi <luca.pezzolesi2@studio.unibo.it>
2026-01-30 12:34:25 +0100

    add RoomManagerStorage tests

---
+++ src/test/java/it/unibo/storage/roommanager/TestRoomManagerStorage.java
@@ -0,0 +1,53 @@
+package it.unibo.storage.roommanager;
+
+import it.unibo.impl.PlayerImpl;
+import it.unibo.impl.RoomManagerImpl;
+import it.unibo.impl.templates.RoomTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.api.Position;
+import it.unibo.api.rooms.RoomManager;
+
+public class TestRoomManagerStorage {
+
+    private RoomManager model;
+
+    @BeforeEach
+    void initializeTest() {
+        model = new RoomManagerImpl(new PlayerImpl(new Position(0, 0)));
+        model.enterNextRoom(new RoomTemplate("id"));
+    }
+
+    @AfterEach
+    void cleanup() {
+        new File("./src/main/resources/roommanager.save.dat").delete();
+    }
+
+    @Test
+    void testSaveAndLoad() throws IOException, ClassNotFoundException {
+        RoomManagerStorage.save(model);
+        File file = new File("./src/main/resources/roommanager.save.dat");
+        assertTrue(file.exists());
+        RoomManager loaded = RoomManagerStorage.load();
+        assertNotNull(loaded);
+        assertEquals(model.getCurrentPosition(), loaded.getCurrentPosition());
+    }
+
+    @Test
+    void testLoadWithoutFile() throws IOException, ClassNotFoundException {
+        assertThrows(FileNotFoundException.class , () -> RoomManagerStorage.load());
+    }
+}
