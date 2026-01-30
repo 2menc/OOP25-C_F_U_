8de71aa16aec94bc60335382f87d6cbb849c89b4
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-01-30 12:06:27 +0100

    merged enigmas saving and loading in one class and deleted temporary test

--- src/main/java/it/unibo/storage/enigma/EnigmaSave.java
+++ src/main/java/it/unibo/storage/enigma/EnigmaSave.java
@@ -9,6 +9,8 @@ import it.unibo.api.enigmas.Enigma;
 import it.unibo.core.GameSettings;
 
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,14 +18,27 @@ import java.util.List;
 /**
 * enigmas loading system
  */
-public class EnigmaSaveLoad {
+public class EnigmaSave {
 
     List<Enigma> enigmas = new ArrayList<>();
     
     /**
      * 0 args constructor
      */
-    public EnigmaSaveLoad() { }
+    public EnigmaSave() { }
+
+    /**
+     * saves the {@code list} of enigmas in {@code enigmas.yml}
+     * @param list the list of enigmas to save
+     */
+    public void saveEnigmas(final List<Enigma> list) {
+        final Yaml yamlWrite = new Yaml();
+        try(FileWriter fw = new FileWriter(GameSettings.YAML_FILES_DEFAULT_PATH.getValue())) {
+            yamlWrite.dump(list, fw);
+        } catch (IOException excep) {
+            excep.printStackTrace();
+        }
+    }
 
     /**
      * loads the enigmas list from {@code enigmas.yml}
@@ -34,8 +49,8 @@ public class EnigmaSaveLoad {
         loadOpt.setTagInspector(tagInsp);
 
         try(final InputStream fis = new FileInputStream(GameSettings.YAML_FILES_DEFAULT_PATH.getValue())) {
-        final Yaml yamlFile = new Yaml(new Constructor(List.class, loadOpt));
-            enigmas = yamlFile.load(fis);
+        final Yaml yamlRead = new Yaml(new Constructor(List.class, loadOpt));
+            enigmas = yamlRead.load(fis);
         } catch (final Exception excep) {
             excep.printStackTrace();
         }
