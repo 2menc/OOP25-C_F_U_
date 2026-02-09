8073aa660793c132ab72b156c2ef01c7dd9f3185
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-02-09 15:13:22 +0100

    improved Position.java and added 2d vectors support for player velocity

--- src/main/java/it/unibo/api/Position.java
+++ src/main/java/it/unibo/api/Position.java
@@ -1,11 +1,59 @@
 package it.unibo.api;
 
 /**
- * simple record to manage player position
- * @param x player's x
- * @param y player's y
+ * simple class that models a 2-dimensional point
+ * @param x x
+ * @param y y
  */
-public record Position(int x, int y) implements java.io.Serializable {
+public class Position  implements java.io.Serializable {
+
+    private double x;
+    private double y;
+
+    
+    /**
+     * constructor
+     * @param x the x
+     * @param y the y
+     */
+    public Position(final double x, final double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * sums this position with the {@link Vector2d} specified
+     * @param v the vector to sum with
+     * @return the new Position2d
+     */
+    public Position sum(final Vector2D v) {
+        return new Position(x + v.getX(), y + v.getY());
+    }
+
+    /**
+     * subtracts this position with the {@link Vector2d} specified
+     * @param v the vector to subtract
+     * @return the new Position2d
+     */
+    public Position sub(final Vector2D v) {
+        return new Position(x - v.getX(), y - v.getY());
+    }
+
+    /**
+     * gets the x param
+     * @return {@code x}
+     */
+    public double getX() {
+        return this.x;
+    }
+
+     /**
+     * gets the y param
+     * @return {@code y}
+     */
+    public double getY() {
+        return this.y;
+    }
 
     @Override 
     public String toString() {
